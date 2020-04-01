da0ec8ce7bcb13b8b9e0e8e8a96f966a21c536fd
Michael Witkowsky <michael.witk@periscopedev.com>
2020-04-01 18:39:17 -0400

    Add message functionality and scroll to bottom

--- src/components/MessagesList/MessagesList.jsx
+++ src/components/MessagesList/MessagesList.jsx
@@ -1,16 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import "./MessagesList.css";
 import Message from "./components/Message";
 import { GlobalContext } from "../../context/GlobalState";
 
 export const MessagesList = () => {
   const { messages } = useContext(GlobalContext);
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () =>
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
   return (
     <div className="messages-list-container">
       {messages.map(message => (
         <Message message={message} key={message.id} />
       ))}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
