de9c6f764394bb805d842e88811524170cebd854
Michael Witkowsky <michael.witk@periscopedev.com>
2020-04-02 09:43:07 -0400

    Control rows on enter. Empty input value on submit

--- src/components/MessagesInput/MessagesInput.jsx
+++ src/components/MessagesInput/MessagesInput.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./MessagesInput.css";
 import { GlobalContext } from "../../context/GlobalState";
 
 export const MessagesInput = () => {
   const [textMessage, setTextMessage] = useState("");
+  const [rows, setRows] = useState(1);
   const { messages, addMessage } = useContext(GlobalContext);
 
   const handleSubmit = event => {
@@ -13,16 +14,34 @@ export const MessagesInput = () => {
       text: textMessage
     };
     addMessage(newMessage);
+    setTextMessage("");
   };
+
+  const rowsHandler = event => {
+    let currentRows = rows;
+
+    if (event.charCode === 13 && rows < 3) {
+      setRows(currentRows + 1);
+    }
+  };
+
+  useEffect(() => {
+    let splitByEnter = textMessage.split("\n");
+    if (splitByEnter.length < 3) {
+      setRows(splitByEnter.length);
+    }
+  }, [textMessage]);
 
   return (
     <form onSubmit={handleSubmit} className="input-container">
-      <input
+      <textarea
         placeholder="Type a message"
-        type="text"
         className="input-textarea"
         onChange={e => setTextMessage(e.target.value)}
         required={true}
+        rows={rows}
+        onKeyPress={e => rowsHandler(e)}
+        value={textMessage}
       />
       <button className="input-button">Send</button>
     </form>
